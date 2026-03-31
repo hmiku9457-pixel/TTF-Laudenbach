2e752bf104f1f358ae22f69dda4246b554f0654b
hmiku9457-pixel <hmiku9457@gmail.com>
2026-03-31 21:42:24 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,29 +6,28 @@ fetch('data/news.json')
     data.forEach((item, i) => {
       const div = document.createElement('div');
       div.classList.add('news-slide');
-
       if (i === 0) div.classList.add('active');
 
+      // Wichtig: href direkt hier im Template Literal
       div.innerHTML = `
-        <img src="${item.image}">
+        <img src="${item.image}" alt="${item.title}">
         <h3>${item.title}</h3>
         <p>${item.text}</p>
-        <a href="${item.link}">Mehr lesen</a>
+        <a href="${item.link}" class="read-more">Mehr lesen</a>
       `;
-
       container.appendChild(div);
     });
 
-    startSlider();
+    startSlider(data.length); // Länge übergeben
   });
 
-function startSlider() {
+function startSlider(length) {
   const slides = document.querySelectorAll('.news-slide');
   let index = 0;
 
   setInterval(() => {
     slides[index].classList.remove('active');
-    index = (index + 1) % slides.length;
+    index = (index + 1) % length;
     slides[index].classList.add('active');
   }, 6000);
 }
