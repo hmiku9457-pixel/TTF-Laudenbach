2b3eea2d3558ba5bb50520a126bc8e39314e8e66
hmiku9457-pixel <hmiku9457@gmail.com>
2026-03-31 21:08:59 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
-const slides = document.querySelectorAll('.news-slide');
-let index = 0;
+const container = document.querySelector('.news-slider');
 
-function nextSlide() {
-  slides[index].classList.remove('active');
-  index = (index + 1) % slides.length;
-  slides[index].classList.add('active');
+fetch('data/news.json')
+  .then(res => res.json())
+  .then(data => {
+    data.forEach((item, i) => {
+      const div = document.createElement('div');
+      div.classList.add('news-slide');
+
+      if (i === 0) div.classList.add('active');
+
+      div.innerHTML = `
+        <img src="${item.image}">
+        <h3>${item.title}</h3>
+        <p>${item.text}</p>
+        <a href="${item.link}">Mehr lesen</a>
+      `;
+
+      container.appendChild(div);
+    });
+
+    startSlider();
+  });
+
+function startSlider() {
+  const slides = document.querySelectorAll('.news-slide');
+  let index = 0;
+
+  setInterval(() => {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+  }, 6000);
 }
-
-setInterval(nextSlide, 6000);
