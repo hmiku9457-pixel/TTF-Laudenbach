f60361b50fd3df027e81a6c33cd1d4fe81545b7b
hmiku9457-pixel <hmiku9457@gmail.com>
2026-04-08 01:57:24 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -105,7 +105,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	function initAnimations() {
 
 		// Alle Elemente auswählen, die animiert werden sollen
-		const elements = document.querySelectorAll('.box, .team-box, .news-slider, .button');
+		const elements = document.querySelectorAll('.box, .team-box, .news-slider, .button', .table-ewigeRangliste tbody tr);
 
 		elements.forEach((el, index) => {
 
